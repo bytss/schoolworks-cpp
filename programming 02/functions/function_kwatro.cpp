ed9c97737c2d2e6a29faa86ba2e294597e885b23
Jefferson Alaiza <97946487+jffrsnalaiza@users.noreply.github.com>
2022-05-17 22:29:58 +0800

    update!

---
+++ programming 02/functions/function_kwatro.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+char lister(int i, float &f){
+
+    cout << "Semester: " << i << endl;
+    if(f >= 1.0 && f <= 1.75)
+        return 'Y';
+     else
+        return 'N';
+
+}
+
+int main(){
+
+    int x;
+    float y;
+
+    cout << "Enter Semester: ";
+    cin >> x;
+
+    cout << "Enter GPA: ";
+    cin >> y;
+
+    if(lister(x, y) == 'Y')
+        cout << "Congratulations!";
+     else
+         cout << "Keep it up!";
+
+    return 0;
+}
