67fb2ca8ccafeaa62bcfe32392d6c8af4061ceb8
Jefferson Alaiza <97946487+jffrsnalaiza@users.noreply.github.com>
2022-05-17 22:55:39 +0800

    Update function_tres.cpp

--- programming 02/functions/function_tres.cpp
+++ programming 02/functions/function_tres.cpp
@@ -6,14 +6,14 @@ void content(string a[]){
 
     cout << "C++ Fundamentals" << endl;
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 6; i++){
         cout << a[i] << "\n";
     }
 }
 
 int main(){
 
-    string book[4] = {"introduction of c++", "history of c++", "fundamentals of c++", "data types and variables"};
+    string book[6] = {"Introduction to c++", "History of C++", "C++ Fundamentals", "Data types and Bariables", "Operators", "Basic Syntax"};
 
     content(book);
     return 0;
